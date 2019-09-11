7c1e2399970f7307fe02a496a44f50932acc82a4
Nataliegonzalez <54905106+Nataliegonzalez@users.noreply.github.com>
2019-09-11 12:35:29 -0400

    Test week3

--- Test.java
+++ Test.java
@@ -3,32 +3,27 @@
  * @author Natalie
  *
  */
-import java.util.Random;
-public class Test {
-	public static void main (String[] args){
-		Random randGen;
-		int num;
-		int num2;
-		
-		
-		randGen = new Random ();
-		num = randGen.nextInt(15) + 1; // generates a random number 1-15
-		num2 = 14;
-		
-		System.out.print(" the number of today is ...  ");
-		System.out.println(num);
-		
-		if (num==num2) 
-			System.out.println(" its a match!!");
-		else 
-			System.out.println(" its not a match");
-		}
-		
-		
-	}		
-				
-				
-				
-				
-	
 
+public class Test {
+	public static void main (String[] args) {
+		
+//		System.out.println(78 + 98);     // 176
+//		System.out.println('a' + 'b');  // 195
+//		System.out.println('a' - 'b'); //-1
+//		System.out.println('0'+'0');  //96
+//		System.out.println('a' + 1); //98
+//		System.out.println('a');      //a
+		
+		int fam = 6;
+		int num = 3;
+		double due = 104.56;
+		String name1 = "Amy";
+		String name2 = "William";
+		
+				
+		System.out.printf("I have %d cousins in my family", fam); 
+		System.out.printf("%n David has to pay %.2f for his light bill", due);
+		
+		System.out.printf("%n %s has %d more homework assignments than %s ", name2, num, name1);
+	}
+}
